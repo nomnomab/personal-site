895b027fc8236186ca5240d357527a7b21e3b1bd
Andrew Burke <nomnomab1@gmail.com>
2020-10-18 22:45:58 -0400

    (init)

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{color: `teal`}}>
+        <Header headerText="Text" />
+        <h1>About Me</h1>
+        <p>Yes</p>
+    </div>
+  )
+}
